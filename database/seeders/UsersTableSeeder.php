ca01dabd7d9beda18501445ae3afb0591d6bd27a
DAICHI <dadegaomu0@gmail.com>
2025-06-08 23:31:51 +0900

    no message

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Users\User;
+
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +16,28 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'over_name' => '山田',
+            'under_name' => '太郎',
+            'over_name_kana' => 'ヤマダ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'yamada@example.com',
+            'sex' => 1,
+            'birth_day' => '2000-01-01',
+            'role' => 1, // 国語教師
+            'password' => Hash::make('password123')
+        ]);
 
+        User::create([
+            'over_name' => '佐藤',
+            'under_name' => '花子',
+            'over_name_kana' => 'サトウ',
+            'under_name_kana' => 'ハナコ',
+            'mail_address' => 'satou@example.com',
+            'sex' => 2,
+            'birth_day' => '2003-03-15',
+            'role' => 4, // 生徒
+            'password' => Hash::make('password123')
+        ]);
     }
 }
